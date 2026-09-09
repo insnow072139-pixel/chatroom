2cbc21478a6537e7425036564bd9ec404a2f7772
insnow072139-pixel <insnow072139@gmail.com>
2026-09-09 21:39:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import { fileURLToPath } from "url";
 const __dirname=path.dirname(fileURLToPath(import.meta.url));
 const app=express();
 app.use(express.static(path.join(__dirname,"dist")));
-app.get("*",(req,res)=>res.sendFile(path.join(__dirname,"dist","index.html")));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 const server=createServer(app);
 const wss=new WebSocketServer({server});
 const clients=new Map();
